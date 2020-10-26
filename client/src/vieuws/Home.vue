1668f422a23ac9558bfba7fffd286bf9d55180aa
ddvos <0931703@hr.nl>
2020-10-26 22:47:10 +0100

    first version online

--- client/src/vieuws/Home.vue
+++ client/src/vieuws/Home.vue
@@ -1,82 +1,15 @@
 <template>
+<div class="background">
 
-<div class="hello">
-  <div class="navbar navbar-dark bg-dark shadow-sm">
-    <div class="container">
-      <a href="#" class="navbar-brand d-flex align-items-center">
-        <strong>Dominique</strong>
-      </a>
-    </div>
-  </div>
-
-  <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="font-weight-light">RC car control</h1>
-        <p class="lead text-muted">bestuur de auto via deze pagina</p>
-        <p>
+   <div class="livefeed">
+      <div class="battery">Batterij: 80%</div>
+      <video mute='true' playsinline autoplay id='v'  ></video> <!--  //v-bind:style="{ 'border': '7px solid'+color1.hex+'' }" -->
+   </div>
   
-        </p>
-      </div>
-  </div>
+  
+ 
 
-  <div class="album py-5 bg-light">
-    <div class="transmit">
-      <h3>Transmit OSC data</h3>
-      <a href="#" class="btn btn-primary">Turn something on</a>
-
-      <br>
-      <div class="wrapper">
-        <input type="range" min="0" max="100" step="1" v-model="value" v-on:input="sendSlider" > 
-        <span v-text="total"></span>
-      </div>
-
-      
-    </div>
-    <div class="receive">
-      <h3>Receive OSC data</h3>
-       <div id="messageArea">
-            <p id="messageLabel">Remote OSC Message:</p>
-
-            <div class="row">
-              <div class="col-2"></div>
-              <div class="col-4">
-                       <!-- <span v-text="OSCMessage"></span>  -->
-                    <p>{{this.address}}</p>
-                      <!-- <p>{{this.message}}</p>  -->
-                    <div class="color" v-bind:style="{ 'background-color': this.message }"></div> 
-              </div>
-              <div class="col-4">
-                 <p>{{this.address2}}</p>
-                 <p>{{this.message2}}</p> 
-              </div>
-               <div class="col-2"></div>
-            </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="broadcasting">
-
-      </div>
-
-       <div class="animation">
-          <video  v-on:mousemove="updateCoordinates" id="myVideo" ref="myVideo" width="650" height="360">
-              <source src="../assets/WORLD_50fps.mp4" type="video/mp4"> 
-                  Your browser does not support the video tag.
-              </video>
-             <p >
-              Coordinates: {{ x }} <br /> 
-             </p>
-
-      </div>
-
-    </div>
-  </div>
-  <div class="text-muted py-5">
-  <div class="container">
-    <p class="mb-1">Domotica control is &copy; Circus family</p>
-  </div>
-</div>
-<button v-on:click="greet">send</button>
+ 
 </div>
   
 </template>
@@ -214,36 +147,25 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+<style lang="scss" scoped>
+
+.background{
+   width: 100%;
+  height: 100%;
 }
 
-.wrapper{
-  padding-top: 20px;
+.livefeed{
+   width: 100%;
+  height: 80vh;
 }
 
-.btn-primary{
-    margin-top: 20px;
+  video{
+   // position: absolute;
+    margin-left: 0;
+     width: 60vw;
+     height: 50vh;
+    background-color: #1e3a42;
+    z-index:1;
 }
-
-.color {
-  margin: auto;
-  width: 50%;
-  width: 80px;
-  background-color: rgba(71, 255, 178, 0.699);
-  height: 80px;
-  
-}
-
-
 
 </style>
