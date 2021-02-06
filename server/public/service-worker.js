4dbab276461c2bffd3e4dba92d8b5ca44c023a37
ddvos <0931703@hr.nl>
2021-02-06 20:56:58 +0100

    changed service worker added a console.log

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -6,6 +6,15 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.setConfig({
      debug: true 
     });
+
+self.addEventListener('activate', evt=>{
+
+    evt.waitUntil(
+        caches.keys().then(keys=>{
+            console.log(keys);
+        })
+    )
+})
     
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
